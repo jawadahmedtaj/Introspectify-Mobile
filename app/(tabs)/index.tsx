09fc4b8bddbd2e29eec8711cfb4a8125c13df75b
jawad <jawad.a@rocketbots.io>
2024-05-25 11:43:12 +0800

    added global type checking

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,20 @@
-import { Text, View } from 'react-native';
+import { Text } from "react-native";
 
-import { Motion } from '@legendapp/motion';
+import { Motion } from "@legendapp/motion";
 
 export default function TabOneScreen() {
   return (
     <Motion.View
       initial={{ opacity: 0, scale: 0 }}
       animate={{ opacity: 1, scale: 1 }}
-      transition={{ type: 'spring', stiffness: 260, damping: 20 }}
+      transition={{ type: "spring", stiffness: 260, damping: 20 }}
       className="flex-1 justify-center items-center bg-slate-100"
     >
       <Text className="font-bold text-xl">Tab One</Text>
       <Motion.View
         initial={{ scaleX: 0 }}
         animate={{ scaleX: 1 }}
-        transition={{ type: 'spring', stiffness: 200, damping: 10 }}
+        transition={{ type: "spring", stiffness: 200, damping: 10 }}
         className="my-8 h-[1px] w-[80%] bg-slate-200"
       />
     </Motion.View>
