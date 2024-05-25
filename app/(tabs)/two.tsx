4a6f8ede4a458e4ae76221676589080be43ae855
jawad <jawad.a@rocketbots.io>
2024-05-25 11:23:53 +0800

    init

---
+++ app/(tabs)/two.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="font-bold text-xl">Tab Two</Text>
+      <View className="my-8 h-[1px] w-[80%] bg-slate-200" />
+    </View>
+  );
+}
---
+++ firebaseConfig.js
@@ -0,0 +1,35 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Optionally import the services that you want to use
+// import {...} from "firebase/auth";
+// import {...} from "firebase/database";
+// import {...} from "firebase/firestore";
+// import {...} from "firebase/functions";
+// import {...} from "firebase/storage";
+
+const apiKey = process.env.EXPO_PUBLIC_API_KEY;
+const authDomain = process.env.EXPO_PUBLIC_AUTH_DOMAIN;
+const databaseURL = process.env.EXPO_PUBLIC_DATABASE_URL;
+const projectId = process.env.EXPO_PUBLIC_PROJECT_ID;
+const storageBucket = process.env.EXPO_PUBLIC_STORAGE_BUCKET;
+const messagingSenderId = process.env.EXPO_PUBLIC_MESSAGING_SENDER_ID;
+const appId = process.env.EXPO_PUBLIC_APP_ID;
+const measurementId = process.env.EXPO_PUBLIC_MEASUREMENT_ID;
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+// For more information on how to access Firebase in your project,
+// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
